9e81459f1cc82ecbcc93784ec8e7521169e9713b
Naga <153531796+Codermvgr@users.noreply.github.com>
2024-02-04 09:29:03 +0530

    HelloUser.java
    To observe the structure and print Hello User that greets the with username

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter your name : ");
+            String name = input.next();
+            System.out.println("Hello " + name);
+        }
+    }
+}
