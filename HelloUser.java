b45246190a50734f2be63d274fef7710e72e78d3
Naga <153531796+Codermvgr@users.noreply.github.com>
2024-02-04 09:35:42 +0530

    HelloUser.java

--- HelloUser.java
+++ HelloUser.java
@@ -1,7 +1,7 @@
 import java.lang.*;
 import java.util.Scanner;
 
-public class HelloWorld {
+public class HelloUser {
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
             System.out.print("Enter your name : ");
