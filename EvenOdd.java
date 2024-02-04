c83dfbeea78ed91aef9ecd1b7f4823dfb805631b
Naga <153531796+Codermvgr@users.noreply.github.com>
2024-02-04 11:31:02 +0530

    EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 
 public class EvenOdd {
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-            System.out.print("Enter an integer : ");
-            int num = input.nextInt();
-            if (num % 2 == 0) {
-                System.out.println(num + " is a Even number");
-            } else {
-                System.out.println(num + " is a Odd number");
-            }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer : ");
+        int num = input.nextInt();
+        evenodd(num);
+        input.close();
+    }
+
+    public static void evenodd(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " is a Even number");
+        } else {
+            System.out.println(num + " is a Odd number");
         }
     }
 }
