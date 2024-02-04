358bf880dccb97c2d822913d043240d7743d3daa
Naga <153531796+Codermvgr@users.noreply.github.com>
2024-02-04 11:29:38 +0530

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,26 +3,25 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-            System.out.print("Enter two numbers : ");
-            float num1, num2;
-            num1 = input.nextFloat();
-            num2 = input.nextFloat();
-            System.out.println("1.Addition 2.Subtraction 3.Multiplication 4.Division ");
-            System.out.print("Enter your choice : ");
-            int choice = input.nextInt();
-            if (choice == 1) {
-                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
-            } else if (choice == 2) {
-                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
-            } else if (choice == 3) {
-                System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
-            } else if (choice == 4) {
-                System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
-            } else {
-                System.out.println("Invalid choice");
-            }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter two numbers : ");
+        float num1, num2;
+        num1 = input.nextFloat();
+        num2 = input.nextFloat();
+        System.out.println("1.Addition 2.Subtraction 3.Multiplication 4.Division ");
+        System.out.print("Enter your choice : ");
+        int choice = input.nextInt();
+        if (choice == 1) {
+            System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        } else if (choice == 2) {
+            System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        } else if (choice == 3) {
+            System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        } else if (choice == 4) {
+            System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        } else {
+            System.out.println("Invalid choice");
         }
-
+        input.close();
     }
 }
