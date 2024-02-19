5d62a5b8c0efce1c93a901c344f5b7be7ed3f6da
Naga <153531796+Codermvgr@users.noreply.github.com>
2024-02-19 15:53:33 +0530

    Update HelloUser.java

--- week5/HelloUser.java
+++ week5/HelloUser.java
@@ -1,12 +1,1 @@
-import java.lang.System;
-import java.util.Scanner;
 
-public class HelloUser {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your name : ");
-        String name = input.nextLine();
-        System.out.println("Hello " + name);
-        input.close();
-    }
-}
