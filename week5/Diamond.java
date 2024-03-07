21daaab9a1e0c5de43cf4905c4dc4fad0ad3b5cf
Naga <153531796+Codermvgr@users.noreply.github.com>
2024-03-07 09:15:18 +0530

    Diamond.java

---
+++ week5/Diamond.java
@@ -0,0 +1,15 @@
+class A {
+}
+class B extends A {
+}
+class C extends A { 
+}
+// java doesn't support multiple inheritance
+class D extends B,C { 
+
+
+}
+public class Diamond {
+    public static void main(String[] args) {   
+    } 
+}
