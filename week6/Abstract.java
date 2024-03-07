6757b3fab523128af8798f731d8d82be165b067f
Naga <153531796+Codermvgr@users.noreply.github.com>
2024-03-07 10:13:39 +0530

    AbsEncapCPP

---
+++ week6/Abstract.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class AccessSpecifierDemo{
+    private int priVar;
+    protected int proVar;
+    public int pubVar;
+    public void setVar(int priValue,int proValue, int pubValue)
+    {
+        this.proVar = proValue;
+        this.priVar = priValue;
+        this.pubVar = pubValue;
+    }
+    public void getVar()
+    {
+        System.out.println("Private variable: " + priVar );
+        System.out.println("Protected variable: " + proVar );
+        System.out.println("Public variable: " + pubVar );
+    }
+
+};
+public class Abstract{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        AccessSpecifierDemo obj = new AccessSpecifierDemo();
+        int a,b,c;
+        System.out.print("Enter the value of private variable :");
+        a = input.nextInt();
+        System.out.print("Enter the vaule of protected variable :");
+        b = input.nextInt();
+        System.out.print("Enter the value of public variable :");
+        c = input.nextInt();
+        obj.setVar(a,b,c);
+        obj.getVar();
+        input.close();
+    }
+}
